333741b55dfe6ffb635aedaa5544b03800d02f18
thenamk20 <namlaophu@gmail.com>
2022-01-24 00:34:19 +0700

    refactor config controller

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const { engine } = require ('express-handlebars');
 
 const app = express()
-const port = 3000
+const port = 4000
+
+const route = require('./routes')
+
+// Middleware
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
-app.use(morgan('combined'))
+  // app.use(morgan('combined'))
 
 // Template engine
 app.engine('hbs', engine({
@@ -19,14 +28,8 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-
-app.get('/', (req, res) => {
-  res.render('home');
-})
-
-app.get('/thenamk20', (req, res) => {
-  res.render('thenam');
-})
+// routes init
+route(app)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
